0fc2c91e0bc6aef9887991d5ce11d5bd0d11c19b
webizfabulous <webizmoxx@gmail.com>
2020-01-16 10:23:31 +0545

    Added usereducer for data fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DataFetchingOne from './components/DataFetchingOne';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 
 function App() {
   return (
     <div className="App">
-     <DataFetchingOne/>
+      { /* <DataFetchingOne/> */ }
+     <DataFetchingTwo/>
     </div>
   );
 }
